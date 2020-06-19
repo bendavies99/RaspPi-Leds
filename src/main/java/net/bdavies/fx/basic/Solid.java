bda4647112ee3b08042ab08ca07ba5b1c0dd1d37
Ben Davies <me@bdavies.net>
2020-06-19 23:17:18 +0100

    fix: Updated rpi-library to match one on maven repo

--- src/main/java/net/bdavies/fx/basic/Solid.java
+++ src/main/java/net/bdavies/fx/basic/Solid.java
@@ -4,12 +4,17 @@ import com.github.mbelling.ws281x.Color;
 import net.bdavies.Strip;
 import net.bdavies.fx.GlobalColorEffect;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Solid extends GlobalColorEffect {
 
     public Solid(Map<String, Object> data) {
         super(data);
+    }
+
+    public Solid() {
+        this(new HashMap<>());
     }
 
     @Override
